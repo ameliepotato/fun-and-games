b2e11ff7a3fa1e44f071ca451e3cdfe0f794a379
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-24 14:59:00 +0100

    #4 - max profit

---
+++ LeetCode/Solutions/Algorithms/MaxProfit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+    public class MaxProfit
+    {
+        public static int Run(int[] nums)
+        {
+            Console.WriteLine($"MaxProfit with: {CommonTools.PrintCollection(nums.ToList())}");
+            int max = 0;
+            int min = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i==0 || nums[i]< min)
+                {
+                    min = nums[i];
+                }
+                else
+                {
+                    continue;
+                }
+
+                int mymax = GetMaxDistance(nums, i);
+                if(mymax > max) 
+                    max = mymax;
+            }
+            return max;
+        }
+
+        public static int GetMaxDistance(int[] nums, int i)
+        {
+            int max = 0;
+            for (int j = i+1; j < nums.Length; j++)
+            {
+                if (nums[j] > nums[i] && (nums[j] - nums[i]) > max )
+                {
+                    max = nums[j]-nums[i];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Tests/MaxProfit.cs
@@ -0,0 +1,98 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class MaxProfit
+    {
+        [Test]
+        public void Test1()
+        {
+            var input = new int[] { 7, 1, 5, 3, 6, 4 };
+            int expected = 5;
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var input = new int[] { 7, 6, 4, 3, 1 };
+            int expected = 0;
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var input = CommonTools.LoadFromFile<int>("TestData\\MaxProfit.txt")?.ToArray();
+
+            Assert.That(input, Is.Not.Null);
+
+            int expected = 999;
+            DateTime start = DateTime.Now;
+            int expectedSeconds = 10;
+
+            Console.WriteLine($"MaxProfit Started at: {start}");
+
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine($"MaxProfit Ended at:   {end}");
+
+            Assert.That(output, Is.EqualTo(expected));
+
+            Assert.That((int)end.Subtract(start).TotalSeconds, Is.LessThan(expectedSeconds));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var input = CommonTools.LoadFromFile<int>("TestData\\MaxProfitALotOfZeros.txt")?.ToArray();
+
+            Assert.That(input, Is.Not.Null);
+
+            int expected = 0;
+            DateTime start = DateTime.Now;
+            int expectedSeconds = 10;
+
+            Console.WriteLine($"MaxProfit Started at: {start}");
+
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine($"MaxProfit Ended at:   {end}");
+
+            Assert.That(output, Is.EqualTo(expected));
+
+            Assert.That((int)end.Subtract(start).TotalSeconds, Is.LessThan(expectedSeconds));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            int[] input = new int[] { 1, 2 };
+            int expected = 1;
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test6()
+        {
+            int[] input = new int[] { 3, 2, 6, 5, 0, 3 };
+            int expected = 4;
+            int output = LeetCode.Algorithms.MaxProfit.Run(input);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+    }
+}
