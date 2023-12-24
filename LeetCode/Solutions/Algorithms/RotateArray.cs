367f4f84f4dcb8b836366c7d4f9305b4f78be040
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-24 11:56:36 +0100

    #3 - rotate array

---
+++ LeetCode/Solutions/Algorithms/RotateArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://leetcode.com/problems/majority-element
+
+namespace LeetCode.Algorithms
+{
+    public class RotateArray
+    {
+        public static void Run(List<int> nums, int k)
+        {
+            CommonTools.PrintCollection(nums);
+            for (int i = 0; i < k; i++)
+                {
+                    RunOnce(nums);
+                }
+            CommonTools.PrintCollection(nums);
+        }
+
+        public static void RunOnce(List<int> nums)
+        {
+            CommonTools.PrintCollection(nums);
+            int first = nums[0];
+            for (int i = 0; i < nums.Count()-1; i++)
+            {
+                {
+                    var temp = nums[i + 1];
+                    nums[i + 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+            nums[nums.Count-1] = first;
+            CommonTools.PrintCollection(nums);
+        }
+    }
+}
---
+++ LeetCode/Tests/RotateArray.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode;
+
+namespace Tests
+{
+    public class RotateArray
+    {
+        [Test] 
+        public void Test1() {
+            var input = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+            var output = new List<int>() { 5, 6, 7, 1, 2, 3, 4 };
+
+            LeetCode.Algorithms.RotateArray.Run(input, k);
+
+            Assert.That(CommonTools.AreEqual(input.ToArray(), output.ToArray()));
+        }
+
+        [Test] 
+        public void Test2() {
+            var input = new List<int>() { -1, -100, 3, 99 };
+            int k = 2;
+            var output = new List<int>() { 3, 99, -1, -100 };
+
+            LeetCode.Algorithms.RotateArray.Run(input, k);
+
+            Assert.That(CommonTools.AreEqual(input.ToArray(), output.ToArray()));
+
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var input = new List<int>() { 1,2 };
+            int k = 1;
+            var output = new List<int>() { 2, 1 };
+
+            LeetCode.Algorithms.RotateArray.Run(input, k);
+
+            Assert.That(CommonTools.AreEqual(input.ToArray(), output.ToArray()));
+
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var input = new List<int>() { 1, 2 };
+            int k = 3;
+            var output = new List<int>() { 2, 1 };
+
+            LeetCode.Algorithms.RotateArray.Run(input, k);
+
+            Assert.That(CommonTools.AreEqual(input.ToArray(), output.ToArray()));
+
+        }
+    }
+}
