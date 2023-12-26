bb2b81b82f2286e99cf328fa2189e20168e61d8b
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-26 13:45:57 +0100

    #6 - randomized set

---
+++ LeetCode/Solutions/Algorithms/RandomizedSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+    public class RandomizedSet
+    {
+        private List<int> _values;
+        public RandomizedSet()
+        {
+            _values = new List<int>();
+        }
+
+        public bool Insert(int val)
+        {
+            if (!_values.Contains(val))
+            {
+                _values.Add(val);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(int val)
+        {
+            if (_values.Contains(val))
+            {
+                _values.Remove(val);
+                return true;
+            }
+            return false;
+        }
+
+        public int GetRandom()
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(-1, _values.Count()-1)+1;
+            return _values.ElementAt(i);
+        }
+    }
+}
---
+++ LeetCode/Tests/RandomizedSet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class RandomizedSet
+    {
+        [Test] 
+        public void Test1()
+        {
+            LeetCode.Algorithms.RandomizedSet obj = new LeetCode.Algorithms.RandomizedSet();
+            int val = 1;
+            bool param_1 = obj.Insert(val);
+            
+            Assert.That(param_1);
+
+            int param_3 = obj.GetRandom();
+
+            Assert.That(param_3, Is.EqualTo(1));
+
+            bool param_4 = obj.Insert(2);
+
+            Assert.That(param_4);
+
+            bool param_5 = obj.Insert(1);
+
+            Assert.That(!param_5);
+
+
+            bool param_2 = obj.Remove(1);
+
+            Assert.That(param_2);
+
+            bool param_6 = obj.Remove(1);
+
+            Assert.That(!param_6);
+
+        }
+    }
+}
